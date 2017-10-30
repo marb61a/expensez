5eb42d147b65ab54f312078463117ea80b2365e8
martin byrne <marb61a@gmail.com>
2017-10-30 16:55:23 +0000

    added AppRouter

--- src/app.js
+++ src/app.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        Dashboard Component
-    </div>   
-);
-
-const AddExpensePage = () => (
-    <div>
-        Expense Component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Expense Edit Component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        Help Component
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            
-        </div>
-    </BrowserRouter>
-);
 
 ReactDOM.render(
-    routes,
+    <AppRouter />,
     document.getElementById('app')
 );
