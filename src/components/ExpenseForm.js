d3ed9d20eb2605d82506f72f97b866abbc5adfb0
martin byrne <marb61a@gmail.com>
2017-11-07 21:19:17 +0000

    updated expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -13,6 +13,38 @@ export default class ExpenseForm extends React.Component{
         error: ''
     }
     
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    }
+    
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    }
+    
+    onAmountChange = (e) => {
+        const amount = this.target.value;
+        
+        if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)){
+            this.setState(() => ({ amount }));    
+        }
+    }
+    
+    onDateChange = (createdAt) => {
+        if(createdAt){
+            this.setState(() => ({ createdAt }));
+        }
+    }
+    
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calenderFocused: focused }));
+    }
+    
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
+    
     render(){
         return(
             <div>
