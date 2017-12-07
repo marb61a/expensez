69150d5997980c61ec511c9b4b04bd610274945a
martin byrne <marb61a@gmail.com>
2017-12-07 12:39:40 +0000

    updated component files

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,26 @@ import { connect } from 'rect-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1> Add Expense</h1>
-    <ExpenseForm 
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  addExpense: (expense) => dispatch(addExpense(expense));
+};
 
 export default connect()(AddExpensePage);
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header>
         <h1>Expensez</h1>
-        <NavLink to="/"  activeClassName="is-active" exact={true}>Dashboard </NavLink>
-        <NavLink to="/create"  activeClassName="is-active" >Create Expenses </NavLink>
-        <NavLink to="/edit"  activeClassName="is-active" >Edit Expenses </NavLink>
-        <NavLink to="/help"  activeClassName="is-active" >Help </NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     </header>
 );
 
