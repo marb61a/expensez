e79347ba2876cff45bceba255f17fd94d935c25a
martin byrne <marb61a@gmail.com>
2017-12-12 13:25:39 +0000

    updated ExpenseListFilter test

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -27,3 +27,45 @@ beforeEach(() => {
 test ('should render ExpenseListFilters correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render ExpenseListFilters with alt data correctly', () => {
+    wrapper.setProps({
+        filters: altFilters    
+    });
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle text change', () => {
+    const value = 'rent';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+    const value = 'date';
+    wrapper.setProps({
+        filters: altFilters    
+    });
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByDate).toHaveBeenCalled();
+});
+
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+    
+});
+
+test('should handle date focus changes', () => {
+    
+});
