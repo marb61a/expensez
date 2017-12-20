89ba1f225b01e10cde6292edec425014a57ae23f
martin byrne <marb61a@gmail.com>
2017-12-20 12:52:08 +0000

    update exoenseformtest file

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -21,5 +21,28 @@ test('should render error for invalid form submission', () => {
 
 test('should set description on input change', () => {
     const value = 'New description';
-    
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
 });
+
+test('should set note on textarea change', () => {
+    const value = 'New note value';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);    
+});
+
+test('should set amount if valid input', () => {
+    const value = '23.50';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+    target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+});
+
