b395590aa22ad2dcce362338fe56fbc46561f646
martin byrne <marb61a@gmail.com>
2017-12-31 19:01:48 +0000

    updating selectors tests

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -35,3 +35,25 @@ test('should filter by endDate', () => {
     expect(result).toEqual([expenses[0], expenses[1]]);
 });
 
+test('should sort by date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    }; 
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test('should sort by amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    };     
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
+
