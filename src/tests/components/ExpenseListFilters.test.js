cdd398e111d4dd9ff1a4942f52324fe2f66db466
martin byrne <marb61a@gmail.com>
2018-02-12 13:53:45 +0000

    updated tests

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -63,9 +63,15 @@ test('should sort by amount', () => {
 });
 
 test('should handle date changes', () => {
-    
+    const startDate = moment(0).add(4, 'years');
+    const endDate = moment(0).add(8, 'years');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
 });
 
 test('should handle date focus changes', () => {
-    
+    const calendarFocused = 'endDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,4 +5,11 @@ import Header from '../../components/Header';
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
